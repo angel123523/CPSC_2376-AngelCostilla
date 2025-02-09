ec4a28df421c282313e7793a0418427beae00d9c
Angel Costilla <angelcostill@gmail.com>
2025-02-09 17:04:11 -0600

    practice 2
    finally got around to finishing practice 2

---
+++ practice02/practice02/practice02.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+const std::string FILENAME = "account_balance.txt";
+
+double readBalanceFile() {
+	std::ifstream inputFile(FILENAME);
+	double balance;
+	if (inputFile) {
+		inputFile >> balance;
+		inputFile.close();
+	}
+	else {
+		balance = 100.00;
+		std::ofstream outputFile(FILENAME);
+		if (outputFile) {
+			outputFile << balance;
+			outputFile.close();
+		}
+		else {
+			std::cerr << "error: Could not create file to store balance.\n";
+			exit(1);
+		}
+	}
+	return balance;
+}
+
+void writeBalanceFile(double balance) {
+	std::ofstream outputFile(FILENAME);
+	if (outputFile) {
+		outputFile << balance;
+		outputFile.close();
+	}
+	else {
+		std::cerr << "error: could not write balance to file,\n";
+		exit(1);
+	}
+}
+
+void checkBalance(double balance) {
+	std::cout << "your curret balance is: $" << balance << "\n";
+	return;
+}
+void deposit(double& balance) {
+	double amount;
+	std::cout << "Enter deposit amount: $";
+	std::cin >> amount;
+	if (amount <= 0) {
+		std::cout << "error: cannot deposit a negitive amount.";
+		return;
+	}
+	balance += amount;
+	writeBalanceFile(balance);
+	std::cout << "deposit was Successful. Your new balance is: $" << balance;
+}
+void withdraw(double& balance) {
+	double amount;
+	std::cout << "enter withdrawal amount $";
+	std::cin >> amount;
+	if (amount <= 0) {
+		std::cout << "error: cannot withdrawal a negitive amount.\n ";
+		return;
+	}
+	if (amount > balance) {
+		std::cout << "error: insufficient funds. your balance is $:" << balance;
+		return;
+	}
+	balance -= amount;
+	writeBalanceFile(balance);
+	std::cout << "withdrawal successful. your new balance is: $" << balance;
+}
+void Menu(double& balance) {
+	int choice;
+	do {
+		std::cout << "\nMenu:\n";
+		std::cout << "1. check balance\n";
+		std::cout << "2. Deposit Money\n";
+		std::cout << "3.Withdraw Money\n";
+		std::cout << "4. exit\n";
+		std::cout << "Select an option:\n";
+		std::cin >> choice;
+
+		if (choice == 1) {
+			checkBalance(balance);
+		}
+		else if (choice == 2) {
+			deposit(balance);
+		}
+		else if (choice == 3) {
+			withdraw(balance);
+		}
+		else if (choice == 4) {
+			std::cout << "Thank you for using the bank system. Goodbye!\n";
+		}
+		else {
+			std::cout << "Invalid choice. Please try again.\n";
+		}
+	} while (choice != 4);
+}
+
+int main() {
+	std::cout << "Welcom to your bank account!\n";
+	double balance = readBalanceFile();
+	Menu(balance);
+	return 0;
+}

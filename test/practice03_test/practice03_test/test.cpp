3d7bb513e9924dc0f4938d873337d0d47e640a39
Angel Costilla <angelcostill@gmail.com>
2025-02-12 21:48:58 -0600

    error attempting to open practice 3 sourece file.
    maybe halfway through practice 3 but im getting a "unable to open source file from the practice03 test project. Ill try again later.

---
+++ test/practice03_test/practice03_test/test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include "practice03.cpp"
+
+TEST(MathUtilsTest, SumRange) {
+  EXPECT_EQ(MathUtils::sumRange(1, 5),10);
+  EXPECT_EQ(MathUtils::sumRange(3, 3), 0);
+  EXPECT_EQ(MathUtils::sumRange(-2, 2), 0);
+ 
+}
+TEST(MathUtilsTest, ContainsNegative) {
+	EXPECT_TRUE(MathUtils::containsNegative({ 1,-2,3 }));
+	EXPECT_FALSE(MathUtils::containsNegative({ 1,2,3 }));
+	EXPECT_FALSE(MathUtils::containsNegative({ }));
+	
+}
+TEST(MathUtilsTest, FindMax) {
+	EXPECT_EQ(MathUtils::findMax({ 1,2,3,4 }),4);
+	EXPECT_EQ(MathUtils::findMax({ 1,2,2 }),2);
+	EXPECT_THROW(MathUtils::findMax({}), std::invalid_argument);
+}
+
+int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}

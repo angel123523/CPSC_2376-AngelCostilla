67d38db620853e415f5c5f14b8bd234be00f8680
Angel Costilla <angelcostill@gmail.com>
2025-02-17 18:22:43 -0600

    test 3 again

---
+++ test/Sample-Test1/Sample-Test1/test.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include "pch.h"
+#include "main.cpp"
+
+
+TEST(MathUtilsTest, SumRange) {
+	EXPECT_EQ(MathUtils::sumRange(1, 5), 10);
+	EXPECT_EQ(MathUtils::sumRange(3, 3), 0);
+	EXPECT_EQ(MathUtils::sumRange(-2, 2), 0);
+
+}
+TEST(MathUtilsTest, ContainsNegative) {
+	EXPECT_TRUE(MathUtils::containsNegative({ 1,-2,3 }));
+	EXPECT_FALSE(MathUtils::containsNegative({ 1,2,3 }));
+	EXPECT_FALSE(MathUtils::containsNegative({ }));
+
+}
+TEST(MathUtilsTest, FindMax) {
+	EXPECT_EQ(MathUtils::findMax({ 1,2,3,4 }), 4);
+	EXPECT_EQ(MathUtils::findMax({ 1,2,2 }), 2);
+	EXPECT_THROW(MathUtils::findMax({}), std::invalid_argument);
+}
